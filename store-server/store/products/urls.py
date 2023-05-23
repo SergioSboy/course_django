73c0834174c4d8583af73274f351e479ca346dae
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-05-23 22:36:21 +0300

    Django_course
    Django

---
+++ store-server/store/products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from products.views import products, basket_add, basket_remove
+
+app_name = 'products'
+urlpatterns = [
+    path('', products, name='index'),
+    path('category/<int:category_id>/', products, name='category'),
+    path('paginator/<int:page_number>/', products, name='paginator'),
+    path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
+    path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove')
+]
---
+++ store-server/store/products/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, HttpResponseRedirect
+from products.models import Product, ProductCategory, Basket
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from users.models import User
+
+
+# Create your views here.
+
+# функция = контроллеры = вьюхи = обработчиик запросов
+
+def index(request):
+    context = {'title': "Store",
+               }
+    return render(request, 'products/index.html', context)
+
+
+def products(request, category_id=None, page=1, page_number = 1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)  # пагинация
+
+    context = {'title': 'Store - Каталог',
+               "products": products_paginator,
+               'categories': ProductCategory.objects.all()
+               }
+    return render(request, 'products/products.html', context)
+
+
+@login_required
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+@login_required
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
