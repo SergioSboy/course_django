b315327f468fb6fd224ffaccb913da18e2937b76
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-12-06 21:22:57 +0300

    [upd] first commit -> install Django

--- mars/mars_service/mars_service/settings.py
+++ mars/mars_service/mars_service/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mars_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mars_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qazwsxedc',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
